05b4b289f366da4ebd7209491967f3d937d3101b
pawan495 <pawanpreetkaur495@gmail.com>
2022-03-27 12:07:56 -0400

    update server

--- server.js
+++ server.js
@@ -9,27 +9,72 @@ function init() {
     console.log("        ❤  Employee Tracker ❤")
     console.log("----------------********-----------------")
     mainMenu()
-
 }
-const PORT = process.env.PORT || 3012;
-const app = express();
-
-// Express middleware
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
-
-// Use apiRoutes
-app.use('/api', apiRoutes);
-
-// Default response for any other request (Not Found)
-app.use((req, res) => {
-  res.status(404).end();
-});
-
-// Start server after DB connection
-db.connect(err => {
-  console.log('Database connected.');
-  app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-  });
-});
+function mainMenu(){
+  inquirer.prompt([
+      {
+          type: "list",
+          name: "choice",
+          message: "What would you like to do?",
+          choices: [ "View Departments",  "View Roles", "View Employees",
+                      "Add Department", "Add Role" ,"Add Employee",
+                      "Update employee role", "Update employee managers",
+                      "View by manager", "View by department",
+                      "Delete employee", "Delete Role", "Delete Department",
+                      "total budget" , "budget by department", " Exit Employee Tracker"],
+                      pageSize: 10
+      }
+  ])
+  .then(response =>{
+      console.log(response)
+      let userChoice = response.choice;
+      console.log(userChoice)
+      switch( userChoice ) {
+          case "View Departments":
+              viewDept();
+               break;
+           case "View Roles":
+              viewRoles();
+               break;
+          case "View Employees":
+              viewEmployees();
+              break;
+           case "Add Department":
+              addDept();
+              break;
+          case "Add Role":
+              addRole();
+              break;
+          case "Add Employee":
+              addEmployee();
+               break;
+          case  "Update employee role":
+              updateEmpRole();
+               break;
+          case "Update employee managers":
+              updateEmpman();
+              break;
+          case "View by manager":
+              viewByman();
+              break;
+          case "View by department":
+              viewbyDept();
+              break;
+          case  "Delete Department":
+              delDept();
+              break;
+          case "Delete Role":
+              delRoles();
+              break;
+           case "Delete employee":
+              delEmp();
+              break;
+          case "total budget":
+              tolBudget();
+               break;
+          case" Exit Employee Tracker":
+              Exit();
+              break;
+  }
+})
+}
